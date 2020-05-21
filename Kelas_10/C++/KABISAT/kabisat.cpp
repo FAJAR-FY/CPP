298a5b6bf5bc45e099417f2b9147308bf9ea7ee7
FAJAR-FY <mugiwaran948@gmail.com>
2020-05-21 19:18:22 +0700

    Kabisat

---
+++ Kelas_10/C++/KABISAT/kabisat.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+main()
+{
+	int z;
+	cout <<"masukan Tahun:";
+	cin >> z;
+	if (z%4==0)
+	cout <<"tahun kabisat";
+	else
+	cout <<"Bukan Tahun Kabisat";
+	getch ();
+}
